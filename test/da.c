54b460fe0ab0888e1af5119ada58c9c4e55eda98
gab462 <62474485+gab462@users.noreply.github.com>
2025-12-11 10:59:58 -0300

    Add tests for da.c

---
+++ test/da.c
@@ -0,0 +1,45 @@
+#include "../cut.c"
+
+int
+main(void)
+{
+    int *numbers = NULL;
+
+    push(&numbers, 10);
+    assert(cap(numbers) == 2);
+    assert(len(numbers) == 1);
+    assert(numbers[0] == 10);
+
+    push(&numbers, 1, 2, 3);
+    assert(cap(numbers) == 8);
+    assert(len(numbers) == 4);
+    assert(numbers[0] == 10
+           && numbers[1] == 1
+           && numbers[2] == 2
+           && numbers[3] == 3);
+
+    assert(pop(&numbers) == 3);
+    assert(len(numbers) == 3);
+    assert(numbers[0] == 10
+           && numbers[1] == 1
+           && numbers[2] == 2);
+
+    swap_delete(&numbers, 0);
+    assert(len(numbers) == 2);
+    assert(numbers[0] == 2 && numbers[1] == 1);
+
+    da_push(&numbers, 0, -1, -2);
+    assert(len(numbers) == 5);
+    assert(cap(numbers) == 8);
+
+    int i = 0;
+    foreach(n, numbers){
+        assert(*n == numbers[i]);
+        i++;
+    }
+    assert(i == 5);
+
+    printf("All tests passed.\n");
+
+    return(0);
+}
