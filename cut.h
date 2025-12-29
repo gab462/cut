f8abde8b7841c78c8930742ff6a104751e1a6ac6
gab462 <62474485+gab462@users.noreply.github.com>
2025-12-29 12:09:31 -0300

    Add cut_unix_millis()

--- cut.h
+++ cut.h
@@ -26,6 +26,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <time.h>
 #include <assert.h>
 
 struct da_header
@@ -242,6 +243,15 @@ q_tail(void *q)
         }                                       \
     }while(0)
 
+static inline
+int64_t
+cut_unix_millis(void)
+{
+    struct timespec ts;
+    timespec_get(&ts, TIME_UTC);
+    return((int64_t)ts.tv_sec * 1000 + (int64_t)ts.tv_nsec / 1000000);
+}
+
 #ifndef CUT_REMOVE_PREFIX
 #define CUT_REMOVE_PREFIX 1
 #endif
@@ -259,6 +269,7 @@ q_tail(void *q)
 #define appendf sb_appendf
 #define enqueue q_enqueue
 #define dequeue q_dequeue
+#define unix_millis cut_unix_millis
 
 #endif
 
